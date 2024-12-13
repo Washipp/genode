0d3c4cfe334bf318dfc6af53758532f322fa2f64
Silas Meier <silas.meier@bluewin.ch>
2024-12-13 14:33:19 +0100

    Actually does not need to be a shared lib. Instead it can be statically linked.

--- repos/afl++/lib/mk/afl_libc.mk
+++ repos/afl++/lib/mk/afl_libc.mk
@@ -1,6 +1,5 @@
 # This file actually contains the build instructions
 # It does not specify the variable TARGET.
-SHARED_LIB = yes
 LIBS += base
 
 SRC_CC += $(notdir $(wildcard $(REP_DIR)/src/*.cc))
