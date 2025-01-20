3f44b6f5fc93f6291bde6cebab982aa1e8191de7
Silas Meier <silas.meier@bluewin.ch>
2025-01-20 08:58:57 +0100

    Increase readabilty

--- repos/afl++/lib/mk/afl_libc.mk
+++ repos/afl++/lib/mk/afl_libc.mk
@@ -2,11 +2,11 @@
 # It does not specify the variable TARGET.
 LIBS += base syscall-linux
 
-IGNORE_FILES = afl-file-system.cc
+IGNORE_CC_FILES = afl-file-system.cc
 ALL_SRC_FILES = $(notdir $(wildcard $(REP_DIR)/src/afl_libc/*.cc))
 
 INC_DIR +=  $(REP_DIR)/include/afl_libc
 
-SRC_CC += $(filter-out $(IGNORE_FILES), $(ALL_SRC_FILES))
+SRC_CC += $(filter-out $(IGNORE_CC_FILES), $(ALL_SRC_FILES))
 
 vpath %.cc $(REP_DIR)/src/afl_libc
