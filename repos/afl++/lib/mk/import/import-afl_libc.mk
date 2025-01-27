d10c2a8e13192a42c5e9b1330aca9309092ee9cc
Silas Meier <silas.meier@bluewin.ch>
2025-01-27 07:46:15 +0100

    Added comments, which values to set

--- repos/afl++/lib/mk/import/import-afl_libc.mk
+++ repos/afl++/lib/mk/import/import-afl_libc.mk
@@ -1,1 +1,8 @@
 # When "afl-libc" is included as a LIBS, this file is automatically imported.
+# I probably want to add these values here:
+
+#LIBS   += afl_libc libc
+
+#CC_OPT += -fplugin=/home/g-user/code/AFLplusplus/afl-gcc-pass.so
+
+#SRC_O += /home/g-user/code/genode/build/x86_64/afl-compiler-rt/afl-compiler-rt.o.o
