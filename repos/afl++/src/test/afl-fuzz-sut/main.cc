63d77d902eeb0e73100164c359d7ee362f6ea57c
Silas Meier <silas.meier@bluewin.ch>
2025-02-03 10:18:31 +0100

    Cleared log message

--- repos/afl++/src/test/afl-fuzz-sut/main.cc
+++ repos/afl++/src/test/afl-fuzz-sut/main.cc
@@ -10,7 +10,7 @@ void Libc::Component::construct(Libc::Env &env)
     (void) env;
     Libc::with_libc([&] () {
         Genode::log("The two errors 'Error: no plugin found for fcntl(199)/write(199)' are expected.");
-        Genode::log("They require ./afl-fuzz to run.");
+        Genode::log("They require ./afl-fuzz to run be running.");
         Genode::log("Instrumentation test succeeded.");
     });
 }
