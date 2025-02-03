07bdd6b45f39ebccd91c7bf45526926405171550
Silas Meier <silas.meier@bluewin.ch>
2025-02-03 10:01:37 +0100

    Unused test

--- repos/afl++/src/test/afl-compiler-rt/main.cc
+++
@@ -1,9 +0,0 @@
-#include "base/component.h"
-#include "base/log.h"
-
-
-void Component::construct(Genode::Env &env)
-{
-    env.exec_static_constructors();
-    Genode::log("Test completed.");
-}
--- repos/afl++/src/test/afl-compiler-rt/target.mk
+++
@@ -1,6 +0,0 @@
-TARGET   = test-afl-compiler-rt
-SRC_CC   += main.cc
-LIBS     += base
-
-CC_OPT += -fplugin=/home/g-user/code/AFLplusplus/afl-gcc-pass.so
-
