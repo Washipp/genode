a07b61f020930d8e255baac402e6c40de09ce84e
Silas Meier <silas.meier@bluewin.ch>
2025-02-04 11:56:43 +0100

    Minimized the instrumentation test.

--- repos/afl++/src/test/afl-fuzz-sut/main.cc
+++ repos/afl++/src/test/afl-fuzz-sut/main.cc
@@ -1,10 +1,6 @@
 #include <libc/component.h>
 #include <base/log.h>
 
-
-/* Used for debugging. Upon call, the execution waits for an ENTER input. */
-extern "C" void wait_for_continue(void);
-
 void Libc::Component::construct(Libc::Env &env)
 {
     (void) env;
