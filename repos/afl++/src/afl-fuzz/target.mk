2fa137ab95c22201a95a1f0f145d88744d9db5fc
Silas Meier <silas.meier@bluewin.ch>
2025-02-05 11:27:03 +0100

    Set the correct vpath.

--- repos/afl++/src/afl-fuzz/target.mk
+++ repos/afl++/src/afl-fuzz/target.mk
@@ -31,5 +31,6 @@ AFL_FUZZ_FILES = $(notdir $(wildcard $(AFL++_DIR)/src/afl-fuzz*.c))
 SRC_C += $(AFL_FUZZ_FILES)
 
 vpath %.c $(AFL++_DIR)/src
+vpath %.cc $(REP_DIR)/src/afl_libc
 vpath %.cc $(REP_DIR)/src/shm
 
