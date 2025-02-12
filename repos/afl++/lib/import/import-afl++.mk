eaa821fc04d077ae9ea04160a3167d18c41fb775
Silas Meier <silas.meier@bluewin.ch>
2025-02-12 12:03:57 +0100

    afl++ now uses the gcc-pass from the contrib repository

--- repos/afl++/lib/import/import-afl++.mk
+++ repos/afl++/lib/import/import-afl++.mk
@@ -1,4 +1,2 @@
-# When "afl++" is included as a LIBS, this file is automatically imported.
-# I probably want to add these values here:
-# TODO: check if the file exists, and link it from the contrib directory.
-CC_OPT += -fplugin=/home/g-user/code/AFLplusplus/afl-gcc-pass.so
+AFL++_DIR := $(call select_from_ports,afl++)/src/app/afl++
+CC_OPT += -fplugin=$(AFL++_DIR)/afl-gcc-pass.so
