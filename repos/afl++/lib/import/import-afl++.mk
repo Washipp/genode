a34e7abb91139915df790422376faeee7e86d0df
Silas Meier <silas.meier@bluewin.ch>
2025-02-18 08:42:34 +0100

    Using afl++ now also adds libc and its headerfiles.

--- repos/afl++/lib/import/import-afl++.mk
+++ repos/afl++/lib/import/import-afl++.mk
@@ -1,2 +1,4 @@
 AFL++_DIR := $(call select_from_ports,afl++)/src/app/afl++
 CC_OPT += -fplugin=$(AFL++_DIR)/afl-gcc-pass.so
+LIBS += libc
+include $(call select_from_repositories,lib/import/import-libc.mk)
