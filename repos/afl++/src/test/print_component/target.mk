f704114c365935d2cbaae32041948be9fd59ddec
Silas Meier <silas.meier@bluewin.ch>
2025-03-18 11:54:42 +0100

    Removed unused components

--- repos/afl++/src/test/print_component/target.mk
+++
@@ -1,8 +0,0 @@
-TARGET   = print_component
-LIBS     += base libc
-SRC_CC   += main.cc shm_client.cc
-
-INC_DIR += $(REP_DIR)/include/afl_libc
-INC_DIR += $(REP_DIR)/include/shm
-
-vpath %.cc $(REP_DIR)/src/shm
--- repos/afl++/src/wrapper/target.mk
+++
@@ -1,5 +0,0 @@
-TARGET = wrapper
-SRC_CC += wrapper.cc
-LIBS += base afl++
-
-SRC_O += /home/g-user/code/genode/build/x86_64/test/sut/main
