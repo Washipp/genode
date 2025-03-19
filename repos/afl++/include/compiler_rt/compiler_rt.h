512beba7d80dd99093b948eed291312062c90ae1
Silas Meier <silas.meier@bluewin.ch>
2025-03-19 07:43:19 +0100

    Type definitions from compiler_rt moved into the header file.

--- repos/afl++/include/compiler_rt/compiler_rt.h
+++ repos/afl++/include/compiler_rt/compiler_rt.h
@@ -4,6 +4,29 @@
 /* Genode includes*/
 #include <base/component.h>
 
+/* afl++ type definitions
+ * They come from types.h and llvm-alternative-coverage.h
+ * This way, wrapper does not need libc.
+ * */
+typedef unsigned char u8;
+typedef unsigned short int u16;
+typedef unsigned int u32;
+typedef unsigned long int u64;
+
+/* Do not change this unless you really know what you are doing. */
+#define MAP_SIZE (1U << MAP_SIZE_POW2)
+#if MAP_SIZE <= 2097152
+#define MAP_INITIAL_SIZE (2 << 20)  // = 2097152
+#else
+#define MAP_INITIAL_SIZE MAP_SIZE
+#endif
+
+/* Maximum ngram size */
+#define NGRAM_SIZE_MAX 16U
+
+/* Maximum K for top-K context sensitivity */
+#define CTX_MAX_K 32U
+
 typedef int (*Function_to_fuzz)(void*, unsigned int *);
 
 extern int call_function(Function_to_fuzz function_to_fuzz);
--- repos/afl++/src/compiler_rt/compiler_rt.cc
+++ repos/afl++/src/compiler_rt/compiler_rt.cc
@@ -16,29 +16,6 @@
 
 using namespace Genode;
 
-/* afl++ type definitions
- * They come from types.h and llvm-alternative-coverage.h
- * This way, wrapper does not need libc.
- * */
-typedef unsigned char u8;
-typedef unsigned short int u16;
-typedef unsigned int u32;
-typedef unsigned long int u64;
-
-/* Do not change this unless you really know what you are doing. */
-#define MAP_SIZE (1U << MAP_SIZE_POW2)
-#if MAP_SIZE <= 2097152
-#define MAP_INITIAL_SIZE (2 << 20)  // = 2097152
-#else
-#define MAP_INITIAL_SIZE MAP_SIZE
-#endif
-
-/* Maximum ngram size */
-#define NGRAM_SIZE_MAX 16U
-
-/* Maximum K for top-K context sensitivity */
-#define CTX_MAX_K 32U
-
 /* the __afl_* variables are required by the instrumented binary and need to be setup before
  * the fuzzing starts. */
 
