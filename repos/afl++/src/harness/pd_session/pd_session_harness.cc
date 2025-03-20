526e870890e4b9e6f8fe39aed362c5856355b384
Silas Meier <silas.meier@bluewin.ch>
2025-03-20 11:07:56 +0100

    Created own run scripts for each session to be tested. Updated pd-session harness

--- repos/afl++/src/harness/pd_session/pd_session_harness.cc
+++ repos/afl++/src/harness/pd_session/pd_session_harness.cc
@@ -19,10 +19,12 @@ void Component::construct(Env &env)
     int exit_code = 0;
 
     /*  First set up the PD session client */
+    // FIXME: maybe a different env.pd_session_cap() is needed?
     Pd_session_client psc = Pd_session_client(env.pd_session_cap());
 
     /* PD signals RPC calls */
     Capability<Signal_source> signal_source;
+    // FIXME: maybe define a different signal_source
     psc.signal_source().with_result(
         [&](Capability<Signal_source> ss) {
             signal_source = ss;
@@ -46,55 +48,99 @@ void Component::construct(Env &env)
         env.parent().exit(0);
     memcpy(&result, get_fuzz_ptr(), sizeof(addr_t));
     Pd_session::Imprint const imprint { result };
+    Capability<Signal_context> context;
     psc.alloc_context(signal_source, imprint).with_result(
             [&](Capability<Signal_context> sc) {
                 Genode::log(sc.local_name());
-                return;
+                context = sc;
             },
             [&](Pd_session::Alloc_context_error error) {
                 switch (error) {
                     case Pd_session::Alloc_context_error::OUT_OF_RAM:
                     case Pd_session::Alloc_context_error::OUT_OF_CAPS:
                     case Pd_session::Alloc_context_error::INVALID_SIGNAL_SOURCE:
-                        exit_code = 0; return;
+                        env.parent().exit(0);
+                        break;
                     default:
-                        exit_code = 1; return;
+                        env.parent().exit(1);
                 }
             }
     );
+    if (!context.valid())
+        env.parent().exit(exit_code);
+
+    psc.submit(context, *get_fuzz_len());
     psc.free_signal_source(signal_source);
-//    psc.free_signal_source();
-//    psc.alloc_context();
-//    psc.free_context();
-//    psc.submit();
+    psc.free_context(context);
 
     /* RPC Framework */
-//    psc.alloc_rpc_cap();
-//    psc.free_rpc_cap();
+    // FIXME: define psc.native_pd()
+    Native_capability native_capability;
+    psc.alloc_rpc_cap(psc.native_pd()).with_result(
+            [&](Native_capability nc) {
+                native_capability = nc;
+            },
+            [&](Pd_session::Alloc_rpc_cap_error error) {
+                switch (error) {
+                    case Pd_session::Alloc_rpc_cap_error::OUT_OF_RAM:
+                    case Pd_session::Alloc_rpc_cap_error::OUT_OF_CAPS:
+                        env.parent().exit(0);
+                        break;
+                    default:
+                        env.parent().exit(1);
+                }
+            }
+    );
+    if (!native_capability.valid())
+        env.parent().exit(exit_code);
+
+    psc.free_rpc_cap(native_capability);
 
     /* Virtual address management */
+    // Nothing to test here, this simply returns capabilities to RM
 //    psc.address_space();
 //    psc.stack_area();
 //    psc.linker_area();
 
     /* Capability allocation */
-//    psc.ref_account();
-//    psc.transfer_quota();
-//    psc.cap_quota();
-//    psc.used_caps();
-//    psc.avail_caps();
+    Pd_connection pd_reference { env };
+
+    psc.ref_account(pd_reference.cap());
+    /* This interprets the first 8 bytes of the pointer as a number. */
+    size_t interpret_as_number = 1024 * 8;
+    ascii_to(((char*) get_fuzz_ptr()), interpret_as_number);
+    Pd_session::Transfer_cap_quota_result cap_transfer_result = psc.transfer_quota(pd_reference.cap(), Cap_quota{interpret_as_number});
+    switch (cap_transfer_result) {
+        case Pd_session::Transfer_cap_quota_result::OK:
+        case Pd_session::Transfer_cap_quota_result::OUT_OF_CAPS:
+        case Pd_session::Transfer_cap_quota_result::INVALID_SESSION:
+        case Pd_session::Transfer_cap_quota_result::NO_REF_ACCOUNT:
+            break;
+        default:
+            env.parent().exit(1);
+    }
 
     /* RAM allocation */
-//    psc.transfer_quota();
-//    psc.ram_quota();
-//    psc.used_ram();
-//    psc.avail_ram();
+    Pd_session::Transfer_ram_quota_result ram_transfer_result = psc.transfer_quota(pd_reference.cap(), Ram_quota{interpret_as_number});
+    switch (ram_transfer_result) {
+        case Pd_session::Transfer_ram_quota_result::OK:
+            break;
+        case Pd_session::Transfer_ram_quota_result::OUT_OF_RAM:
+            break;
+        case Pd_session::Transfer_ram_quota_result::INVALID_SESSION:
+        case Pd_session::Transfer_ram_quota_result::NO_REF_ACCOUNT:
+            exit_code |= 1;
+            break;
+        default:
+            break;
+    }
 
     /* kernel specific interface */
-//    psc.native_pd();
+    // Nothing to be done here
 
     /* System management interface */
-//    psc.system_control_cap();
+    // FIXME: Not sure on how to use this...
+//    psc.system_control_cap(Affinity::Location());
 
     /* user-level device drivers */
 //    psc.dma_addr();
